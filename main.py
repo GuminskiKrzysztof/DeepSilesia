dfa2fe42dd44dfac3c6bf3e903c455603db52bbe
Asia <jw302114@student.polsl.pl>
2024-09-28 18:32:45 +0200

    Adding frontend 2

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def html():
+    return render_template("index.html")
+
+@app.route("/playground")
+def playground():
+    return render_template("playground.html")
+
+@app.route("/manual")
+def manual():
+    return render_template("manual.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
