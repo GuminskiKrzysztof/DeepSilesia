68aac08e8eb91d99a103075a7305e5ed83200f90
GuminskiKrzysztof <krzysztof.guminski629@gmail.com>
2024-09-28 18:08:19 +0200

    iris

---
+++ iris.py
@@ -0,0 +1,104 @@
+import io
+import numpy as np
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, send_file
+from IPython.display import clear_output
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import RealAmplitudes, ZZFeatureMap
+from qiskit_algorithms.optimizers import COBYLA, L_BFGS_B
+from qiskit_algorithms.utils import algorithm_globals
+
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier, VQC
+from qiskit_machine_learning.algorithms.regressors import NeuralNetworkRegressor, VQR
+from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
+from qiskit_machine_learning.circuit.library import QNNCircuit
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import MinMaxScaler
+from qiskit.circuit.library import RealAmplitudes
+from qiskit.primitives import Sampler
+
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+import time
+from qiskit_machine_learning.algorithms.classifiers import VQC
+
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from qiskit_algorithms.utils import algorithm_globals
+
+algorithm_globals.random_seed = 123
+
+
+iris_data = load_iris()
+print(iris_data.DESCR)
+
+features = iris_data.data
+labels = iris_data.target
+
+
+df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+df["class"] = pd.Series(iris_data.target)
+
+sns.pairplot(df, hue="class", palette="tab10")
+
+num_features = features.shape[1]
+train_features, test_features, train_labels, test_labels = train_test_split(
+    features, labels, train_size=0.8, random_state=algorithm_globals.random_seed
+)
+
+feature_map = ZZFeatureMap(feature_dimension=num_features, reps=1)
+feature_map.decompose().draw(output="mpl", style="clifford", fold=20)
+
+ansatz = RealAmplitudes(num_qubits=num_features, reps=3)
+ansatz.decompose().draw(output="mpl", style="clifford", fold=20)
+
+from qiskit_algorithms.optimizers import COBYLA
+
+optimizer = COBYLA(maxiter=100)
+
+
+sampler = Sampler()
+
+
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+    
+
+
+vqc = VQC(
+    sampler=sampler,
+    feature_map=feature_map,
+    ansatz=ansatz,
+    optimizer=optimizer,
+    callback=callback_graph,
+)
+
+# clear objective value history
+objective_func_vals = []
+
+start = time.time()
+vqc.fit(train_features, train_labels)
+elapsed = time.time() - start
+
+print(f"Training time: {round(elapsed)} seconds")
+
+train_score_q4 = vqc.score(train_features, train_labels)
+test_score_q4 = vqc.score(test_features, test_labels)
+
+print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
+print(f"Quantum VQC on the test dataset:     {test_score_q4:.2f}")
+
