e9ec7a22541569c3a4c92d61ff4048c41e917535
Veragath <135152088+Veragath@users.noreply.github.com>
2024-09-29 11:48:05 +0200

    Update chatbot.js

--- static/scripts/chatbot.js
+++ static/scripts/chatbot.js
@@ -1,8 +1,8 @@
 function toggleText() {
     var bubbleMessage = document.getElementById("bubbleMessage");
     if (bubbleMessage.style.display === "none" || bubbleMessage.style.display === "") {
-        bubbleMessage.style.display = "block"; // Show 
+        bubbleMessage.style.display = "block";
     } else {
-        bubbleMessage.style.display = "none"; // Hide 
+        bubbleMessage.style.display = "none";
     }
 }
